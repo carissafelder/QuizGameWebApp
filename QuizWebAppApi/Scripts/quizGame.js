5692d3a8559f31ccff5bedc725b9caf311d1abe7
Carissa Felder <carissa.felder@gmail.com>
2017-03-23 14:22:27 -0400

    Implemented the javascript files

--- QuizWebAppApi/Scripts/quizGame.js
+++ QuizWebAppApi/Scripts/quizGame.js
@@ -1,41 +1,46 @@
 ﻿$(document).ready(function () {
     var nextQuestionButton = document.getElementById('next-question-button'),
+        quizResponse = document.getElementById('quiz-response'),
         questionArea = document.getElementById('question-area'),
         currentQuestionId;
 
-    var clickHandler = function clickHandler(event){
+    var clickHandler = function clickHandler(event) {
 
-        var answerSubmission = { questionId: currentQuestionId, answerId: event.currentTarget.id};
-        
+        var answerSubmission = { questionId: currentQuestionId, answerId: event.currentTarget.id };
+
         $.getJSON("/api/quizquestion/isanswercorrect/", answerSubmission)
             .done(function (data) {
                 if (data.IsCorrect) {
-                    alert("Congratulations! That's right");
-                }else {
-                    alert("Sorry! That wasn't correct!");
+                    quizResponse.innerText ="Congratulations! That's right!";
+                } else {
+
+                    quizResponse.innerText="Sorry! That wasn't correct!";
                 }
             })
             .fail(function (jgxhr, textStatus, error) {
-                alert("Whoops! Something went wrong!");
+                quizResponse.innerText="Whoops! Something went wrong!";
             });
 
     }
-    nextQuestionButton .addEventListener('click', function () {
+    nextQuestionButton.addEventListener('click', function () {
         $.getJSON('/api/quizquestion', function (data) {
             var questionDiv = document.createElement('div');
             questionArea.innerHTML = "";
             currentQuestionId = data.QuestionId;
             questionDiv.id = currentQuestionId;
+            questionDiv.classList.add("question");
             questionDiv.innerText = data.Content;
             questionArea.appendChild(questionDiv);
 
             data.Answers.forEach(function (val) {
                 var answerDiv = document.createElement('div');
+                
                 answerDiv.innerText = val.Content;
-                answer.Div.id = val.AnswerId;
+                answerDiv.id = val.AnswerId;
+                answerDiv.classList.add("answer");
                 answerDiv.addEventListener('click', clickHandler);
                 questionArea.appendChild(answerDiv);
             });
         });
     });
-});
+})
